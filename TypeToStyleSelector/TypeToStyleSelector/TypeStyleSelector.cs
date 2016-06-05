994c2ea92264e1ee2b5977867e4204e24fdac681
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-05 18:18:49 +0200

    Initialize implementation

---
+++ TypeToStyleSelector/TypeToStyleSelector/TypeStyleSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TypeToStyleSelector
+{
+    public class TypeStyleSelector : StyleSelector
+    {
+        public List<TypeToStyle> TypeToDataTemplates { get; set; } = new List<TypeToStyle>();
+
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            var type = item.GetType();
+            var typeToDataTemplate = TypeToDataTemplates.FirstOrDefault(t => t.Type == type);
+            return typeToDataTemplate?.Style;
+        }
+    }
+}
---
+++ TypeToStyleSelector/TypeToStyleSelector/TypeToStyle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+
+namespace TypeToStyleSelector
+{
+    public class TypeToStyle
+    {
+        public Type Type { get; set; }
+
+        public Style Style { get; set; }
+    }
+}
